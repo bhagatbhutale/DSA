3ebd1da4c0f604e2799ea9743694fa87dbf3a852
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-21 11:39:15 +0530

    DSA leraned for Internshala

---
+++ Array/1.js
@@ -0,0 +1,23 @@
+// let arr2 = new Array(10, 20, 30, 40);
+
+// console.log(arr2)
+// console.log(arr2[1]);
+
+let arr1 = [20, 40, 30 ,40];
+console.log(arr1);
+
+arr1.push(50);
+console.log(arr1);
+
+arr1.pop()
+console.log(arr1);
+
+arr1.shift()
+console.log(arr1)
+
+
+arr1.unshift(-1)
+console.log(arr1)
+
+arr1[10] = "hi"
+console.log(arr1.length)
---
+++ Array/2.js
@@ -0,0 +1,7 @@
+// Iterating a Array 
+
+let arr1 = [10,20, 30, 40, 50]
+
+for(let i = 0; i< arr1.length; i++) {
+    console.log(arr1[i])
+}
